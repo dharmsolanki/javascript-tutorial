91331a401dd6b7995fc962146fd06267d70c9f25
Dharm Solanki <dharm.solanki@drcsystems.com>
2024-12-27 15:48:54 +0530

    learning fetch

--- basics/promises.js
+++ basics/promises.js
@@ -37,3 +37,28 @@ async function consumePromise(){
 }
 
 consumePromise();
+
+
+async function getAllUsers() {
+    try {
+        const response = await fetch('https://jsonplaceholder.typicode.com/users');
+        const data = await response.json();
+        console.log(data)
+    } catch (error) {
+        console.log(error);
+    }
+    
+}
+
+getAllUsers();
+
+fetch("https://jsonplaceholder.typicode.com/users")
+.then((response) => {
+    return response.json();
+})
+.then((data) =>{
+    console.log(data);
+})
+.catch((error) => {
+    console.log(error)
+})
